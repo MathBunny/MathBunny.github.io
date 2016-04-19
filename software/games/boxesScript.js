5131744ab0b4295e857cdbc8146b50fc1428b81b
MathBunny <horatiulazu@gmail.com>
2016-04-19 15:51:08 -0400

    Update boxes margins

--- software/games/boxesScript.js
+++ software/games/boxesScript.js
@@ -1,4 +1,5 @@
 function updateDimensions (){
-       c.width = document.getElementById("canvas-panel").offsetWidth-40;
-       c.height = c.width * 0.75;
+       console.log("Padding: ",document.getElementById("canvas-panel").style.marginRight*2);
+       c.width = document.getElementById("canvas-panel").offsetWidth - 50; //sketchy, fix ... 
+       c.height = c.width * 0.60;
 }
