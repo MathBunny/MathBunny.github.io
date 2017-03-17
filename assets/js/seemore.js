a9a2c7509e87a77839d1ef97331c408e2653e8e7
MathBunny <horatiulazu@gmail.com>
2017-03-17 18:14:28 -0400

    Add education and awards section to main page

---
+++ assets/js/seemore.js
@@ -0,0 +1,27 @@
+$('.SeeMore2').click(function(){ /* function lets you see more or less text .. hope it works on all browsers :p i should move this to another folder*/
+        var $this = $(this);
+        $this.toggleClass('SeeMore2');
+        if($this.hasClass('SeeMore2')){
+            $this.text('See More');         
+        } else {
+            $this.text('See Less');
+        }
+    });
+$('.SeeMore3').click(function(){ /* function lets you see more or less text .. hope it works on all browsers :p i should move this to another folder*/
+        var $this = $(this);
+        $this.toggleClass('SeeMore3');
+        if($this.hasClass('SeeMore3')){
+            $this.text('See More');         
+        } else {
+            $this.text('See Less');
+        }
+    });
+$('.SeeMore4').click(function(){ /* function lets you see more or less text .. hope it works on all browsers :p i should move this to another folder*/
+        var $this = $(this);
+        $this.toggleClass('SeeMore4');
+        if($this.hasClass('SeeMore4')){
+            $this.text('See More');         
+        } else {
+            $this.text('See Less');
+        }
+    });
